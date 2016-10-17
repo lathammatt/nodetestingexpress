f65bc313d54bf3469145941eaaa4ce35a6efc110
Matt Latham <latham75@gmail.com>
2016-10-17 12:25:07 -0500

    further simple test setup

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get('/', (req, res) => res.send({}))
+app.post('/addresses', (req, res) => res.status(201).send({ _id: 'abc' }))
+app.delete('/addresses/:id', (req, res) => res.sendStatus(204))
 
 module.exports = app
